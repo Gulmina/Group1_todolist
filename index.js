54d359789cc724b3d6a7621b716db9c83e924362
gulmina <gulmina44@gmail.com>
2024-05-15 22:30:36 +0200

    modified

--- index.js
+++ index.js
@@ -28,12 +28,62 @@ form.addEventListener("submit",(e)=>{
 
 const showTodos=()=>{
     const todosHtml = todos().map((todo)=>{
-        return `<li>${todo} X</li>`
-    }).join("")
+        
+        let res= `<li>${todo}</li>`+"<button id=btn value="+`${todo}`+" onClick=edit()>e</button>"
+
+        return res
+    }).join()
     list.innerHTML = todosHtml
+    
+    //const x=document.getElementById("btn").addEventListener("click", edit());
 }
 
 showTodos()
 
+////Edit Function/////////////////
+
+function form2(){
+    alert("hello");
+    edit_form.innerHTML= ` <form id="form2">
+    <input type="text" id="input2"/>
+    <button id="edit-btn">Enter</button>
+</form>`
+
+}
+
+function edit(){
+    x=document.getElementById("btn");
+console.log(x);
+
+    
+    form2()
+ 
+
+    let b=localStorage.getItem("todos");
+    const c=JSON.parse(b);
+    //console.log(c);
+
+    let i=c.indexOf("gul");
+    console.log(i);
+    
+    c[i]="apple";
+    //console.log(c);
+    
+    localStorage.setItem("todos",JSON.stringify(c))
+    console.log(c);
+    input.value="";
+    showTodos()
+     
+
+
+
+// const l=localStorage.getItem(x);
+// console.log(l);
+ 
+   // const index= localTodos.indexOf(x);
+    //console.log(index);
+  // newvalue(x.value);
+    
+}
 
 
