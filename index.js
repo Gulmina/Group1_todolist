1548083ed9790d3ff23de602eb21942c804631aa
Erdinc Yasar <erdinc.y96hotmail.com>
2024-05-16 13:22:28 +0200

    remove btn is working

--- index.js
+++ index.js
@@ -1,39 +1,42 @@
-const form=document.getElementById("form");
-const input=document.getElementById("input");
-const list=document.getElementById("list");
+const form = document.getElementById("form");
+const input = document.getElementById("input");
+const list = document.getElementById("list");
 
-
-const todos=()=>{
-    const todos=localStorage.getItem("todos")
-    return todos ? JSON.parse(todos) : []
+const handleRemove = (index) => {
+    const localTodos = todos();
+    localTodos.splice(index, 1);
+    localStorage.setItem("todos", JSON.stringify(localTodos));
+    showTodos(); 
 }
 
+const todos = () => {
+    const todos = localStorage.getItem("todos");
+    return todos ? JSON.parse(todos) : [];
+}
 
-form.addEventListener("submit",(e)=>{
-    e.preventDefault()
-    if (input.value==="") {
-        alert("please fill in the input!")
-        return
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if (input.value === "") {
+        alert("please fill in the input!");
+        return;
     }
-    const localTodos=todos()
+    const localTodos = todos();
 
-    localTodos.push(input.value)
-    localStorage.setItem("todos",JSON.stringify(localTodos))
+    localTodos.push(input.value);
+    localStorage.setItem("todos", JSON.stringify(localTodos));
     console.log(localTodos);
-    input.value="";
-    showTodos()
+    input.value = "";
+    showTodos();
 })
 
-
-
-const showTodos=()=>{
-    const todosHtml = todos().map((todo)=>{
-        return `<li>${todo} X</li>`
-    }).join("")
-    list.innerHTML = todosHtml
+const showTodos = () => {
+    const todosHtml = todos().map((todo, index) => {
+        return `<li>
+            ${todo} 
+            <button onclick="handleRemove(${index})">Remove</button>
+        </li>`;
+    }).join("");
+    list.innerHTML = todosHtml;
 }
 
-showTodos()
-
-
-
+showTodos();
