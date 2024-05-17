b15a2bd5707ea82c7421dbb0fb98184fce915c6d
Vladimir <gerov.vladimir@gmail.com>
2024-05-17 13:12:42 +0200

    added syling, removed commas,

--- index.js
+++ index.js
@@ -28,10 +28,19 @@ form.addEventListener("submit",(e)=>{
 // added index identifier for the function to target specific items
 // onclick atribute added to call deleteTodo function
 const showTodos=()=>{
-    const todosHtml = todos().map((todo, index)=>{
-        return `<li>${todo} <button onclick="deleteTodo(${index})" >Delete</button></li>`
+    const todosHtml = todos().map((todo,index)=>{
+      
+       
+        let res= `<li><input type="checkbox" id="mark" value='${todo}' > ${todo} <button id=btn value='${todo}' onClick='edit(this,${index})'>Edit</button>
+        <button onclick="deleteTodo(${index})">Delete</button> </li>`
+  
+        return res
     }).join("")
-    list.innerHTML = todosHtml
+
+    
+    list.innerHTML=todosHtml
+    
+    //const x=document.getElementById("btn").addEventListener("click", edit());
 }
 
 // Function to delete a todo from the list
@@ -46,4 +55,79 @@ const deleteTodo = (index) => {
 showTodos()
 
 
+ 
+const mark=document.getElementById("mark");
+mark.addEventListener("click",(e)=>{
+    e.preventDefault()
+    console.log(123)
+    let a=document.getElementById("mark").checked;
+   
+    });
 
+ 
+
+////Edit Function/////////////////
+
+ function form2(value){
+   // alert("hello");
+   
+  
+     list.innerHTML=`<li>${value}<form id="form2">
+    <input type="text" id="input2"/>
+    <button id="btn2">Save</button>
+</form></li>` 
+ 
+
+} 
+
+function edit(a,index){
+    //x=document.getElementById("btn");
+    console.log(a.value);
+    console.log(index);
+
+    //let value_index=a.value;
+    //console.log(value_index)
+
+   form2(a.value)
+
+   
+
+
+    const h=document.getElementById("btn2").addEventListener("click", (e)=>{
+        e.preventDefault();
+
+
+        let b=localStorage.getItem("todos");
+
+        const c=JSON.parse(b);
+        //const newvalue=input2.value;
+        //console.log(newvalue);
+        //console.log(b);
+    
+       // let i=c.indexOf(a.value);
+
+        //console.log(i);
+        
+        c[index]=input2.value;
+        //console.log(c);
+        
+        localStorage.setItem("todos",JSON.stringify(c))
+        console.log(c);
+        edit_form.innerHTML=""
+        showTodos()
+    });
+
+}
+
+
+
+    
+
+
+
+// const l=localStorage.getItem(x);
+// console.log(l);
+ 
+   // const index= localTodos.indexOf(x);
+    //console.log(index);
+  // newvalue(x.value);
